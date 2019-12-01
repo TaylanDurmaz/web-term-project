0133d465bd6cb4d2fcb15f96ef3bf9fd90a22467
Taylan Durmaz <merttaylandurmaz@gmail.com>
2019-12-01 11:18:33 +0300

    auth reducer fix

--- src/redux/auth/reducer.js
+++ src/redux/auth/reducer.js
@@ -1,40 +1,8 @@
 import { types } from "./actions";
 
 const INITIAL_STATE = {
-  user: {
-    _id: "5ddfd151ddbc8082d02cad4d",
-    role: 2,
-    email: "merttaylandurmaz@gmail.com",
-    password: "qwerty",
-    name: "Mert Taylan",
-    surname: "Durmaz",
-    phoneNumber: "05555555555",
-    tokens: [
-      {
-        _id: { $oid: "5de00c305ffc7193d1be7512" },
-        token:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZGRmZDE1MWRkYmM4MDgyZDAyY2FkNGQiLCJpYXQiOjE1NzQ5NjQyNzJ9.7y3dSyNMTh05EVa78QBRTSIgSRQYgl-3D37r7x6ZD3Q"
-      },
-      {
-        _id: { $oid: "5de00cc6cc5bf99428e16796" },
-        token:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZGRmZDE1MWRkYmM4MDgyZDAyY2FkNGQiLCJpYXQiOjE1NzQ5NjQ0MjJ9.GDpd6NFn0eQcHUXLqvvPh8BGc0DMYPomZBEf8jSKBPo"
-      },
-      {
-        _id: { $oid: "5de06af55ea77aacf7c06179" },
-        token:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZGRmZDE1MWRkYmM4MDgyZDAyY2FkNGQiLCJpYXQiOjE1NzQ5ODg1MzN9.0EYP8cRSo_h3HZ8c-KM8SUsSrKCr_R1tuuyOaQ8aXlw"
-      },
-      {
-        _id: { $oid: "5de070245ea77aacf7c06185" },
-        token:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZGRmZDE1MWRkYmM4MDgyZDAyY2FkNGQiLCJpYXQiOjE1NzQ5ODk4NjB9.bfZWZBp_Z2TbquEtrDdmrwIkduLoGxbTWFqkQURmLAU"
-      }
-    ],
-    __v: { $numberInt: "4" },
-    ownerOf: { $oid: "5ddfd151ddbc8082d02cad4e" }
-  },
-  loggedIn: true
+  user: null,
+  loggedIn: false
 };
 
 function authReducer(state = INITIAL_STATE, action) {
